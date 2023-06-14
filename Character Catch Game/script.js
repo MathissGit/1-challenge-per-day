0719d2a68cc013b4bbcf23d8016e69f39fe3e848
mathis <mathissineux@gmail.com>
2023-06-14 09:18:25 +0200

    Rectification du script:  bug de temps et size caractere

--- Character Catch Game/script.js
+++ Character Catch Game/script.js
@@ -1,10 +1,10 @@
 const screens = document.querySelectorAll('.screen');
 const choose_character_btn = document.querySelectorAll('.choose-character-btn');
-const start_btn = document.getElementById('start-btn');
-const game_container = document.getElementById('game-container');
-const timeEl = document.getElementById('time');
-const scoreEl = document.getElementById('score');
-const message = document.getElementById('message');
+const start_btn = document.getElementById('start-btn')
+const game_container = document.getElementById('game-container')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('Score')
+const message = document.getElementById('message')
 let seconds = 0
 let score = 0
 let selected_characters = {}
@@ -14,7 +14,25 @@ start_btn.addEventListener('click', () => screens[0].classList.add('up'))
 choose_character_btn.forEach(btn => {
     btn.addEventListener('click', () => {
         const img = btn.querySelector('img')
+        const id = btn.getAttribute('id')
         const src = img.getAttribute('src')
+        switch (id) {
+            case 'Michael':
+                img.setAttribute('src', 'image/Michael-Scott.png')
+                img.setAttribute('alt', 'Michael Scott')
+                break;
+            case 'Kevin':
+                img.setAttribute('src', 'image/kevin-malone.png')
+                img.setAttribute('alt', 'Kevin Malone')
+                break;
+            case 'Dwight':
+                 img.setAttribute('src', 'image/dwight-schrute.png')
+                 img.setAttribute('alt', 'Dwight Fart Schrute')
+                 break;
+            default:
+                break;
+        }
+        
         const alt = img.getAttribute('alt')
         selected_characters = { src, alt }
         screens[1].classList.add('up')
@@ -30,9 +48,9 @@ function startGame() {
 function increaseTime() {
     let m = Math.floor(seconds / 60)
     let s = seconds % 60
-    m = m < 10 ? '0${m}' : m 
-    s = s < 10 ? '0${s}' : s
-    timeEl.innerHTML = 'Time: ${m}:${s} '
+    m = m < 10 ? `0${m}` : m 
+    s = s < 10 ? `0${s}` : s
+    timeEl.innerHTML = `Time: ${m}:${s}`
     seconds++
 }
 
@@ -42,30 +60,32 @@ function createCharacter() {
     const { x, y } = getRandomLocation()
     character.style.top = `${y}px`
     character.style.left = `${x}px`
-    character.innerHTML = '<img src="${selected_characters.src}" alt="${selected_characters.alt}" style="transform: rotate(${Math.random() * 360}deg)" />'
+    character.innerHTML = `<img src="${selected_characters.src}" alt="${selected_characters.alt}" style="transform: rotate(${Math.random() * 360}deg)" />`
 
     character.addEventListener('click', cacthCharacter)
 
     game_container.appendChild(character)
+    increaseScore();
 }
 
 function getRandomLocation() {
     const width = window.innerWidth
     const height = window.innerHeight
-    const x = Math.random() * (width - 200) + 100
-    const y = Math.random() * (height - 200) + 100
+    const x = Math.floor(Math.random() * (width - 200) + 100)
+    const y = Math.floor(Math.random() * (height - 200) + 100)
     return { x, y }
 }
 
 function cacthCharacter() {
     setTimeout(createCharacter, 1000)
     setTimeout(createCharacter, 1500)
+    document.querySelector('catch')
 }
 
 function increaseScore() {
     score++
-    if(score > 15) {
-        message.classList('visible')
+    if(score > 19) {
+        message.setAttribute('class','visible')
     }
-    scoreEl.innerHTML = 'score: ${score}'
+    scoreEl.innerHTML = `score: ${score}`
 }
